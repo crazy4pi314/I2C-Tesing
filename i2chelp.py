c22431f83b36a83ac03f317c0d1dbd71beddf7e6
crazy4pi314 <crazy4pi314@gmail.com>
2016-06-15 16:43:12 -0400

    adding the run function and update

--- i2chelp.py
+++ i2chelp.py
@@ -2,16 +2,12 @@
 Helper functions for I2C interfacing
 Sarah Kaiser
 Created: Mon Feb 3
-Last updated: Mon Feb 3
+Last updated: Fri Mar 28
 
 '''
 import sys
 import math
 import array
-print"\n\nStarting..."
-
-
-
 
 '''
 General Utilites
@@ -26,9 +22,13 @@ def Rthermistor10k(temp):
 def Tthermistor10k(R):
 	return (326.3457990944156-273.15*math.log(0.0001*R))/(13.053831963776624+math.log(0.0001*R))
 
-#This changes a passed int into the 4 character hex string representing that number
-def int2phex(intval):
-   		hexval=hex(intval)[2:].zfill(4).upper()   		
+#This changes a passed int into the n character hex string representing that number
+def int2phex(intval,n):
+   		hexval=hex(intval)[2:].zfill(n).upper()   		
+   		return hexval
+#This changes a passed int into the 2 character hex string representing that number
+def int2phex2(intval):
+   		hexval=hex(intval)[2:].zfill(2).upper()   		
    		return hexval
 
 #This changes a voltage into a codeword
@@ -36,30 +36,61 @@ def v2code(v):
    		code=int(round((v*4096)/(2.5-lsb)+lsb))   		
    		return code
 
+#switch to 10 bit readout 
+def hex10bitcode(hex16):
+	return int(bin(int(hex16,16))[2:].zfill(16)[4:],2)
+
+#convert 10 bit codeword to voltage
+def code2volt(code10bit,adcrange):
+	return ((adcrange-2*lsb)/(4096))*code10bit+lsb
+
+
+
+
+#read data to a string
+def list2string(list):
+	return "".join(map(int2phex2,list))
+	
 
 '''
 DAC converting functions
 '''
 # Convert User HV to hex code
-def volt2hex(HVval):
+def volt2hex(HVval,channel):
 	# Check for invalid voltages
 	if (HVval > 500) or (HVval < 0):
-		print "User error, stupid voltage choice. Does not compute. Please try again."	
+		print "User error, stupid voltage choice. Does not compute. Please try again."
+		code='0000'	
 		return '0000'
 	else:
-   		code=int2phex(int(round(((2103.09-703.78)/200)*HVval+4.125))) 		
+		
+		if (channel=='1'):
+			code=int2phex(max(int(round(7.41129*HVval-10.922907)),0),4)
+
+   		elif (channel=='2'):
+			code=int2phex(max(int(round(7.3529590*HVval-7.1530270)),0),4) 
+
+		elif (channel=='3'):
+			code=int2phex(max(int(round(7.3563918*HVval-9.2322928)),0),4) 
+
+		elif (channel=='4'):
+			code=int2phex(max(int(round(7.3705900*HVval-5.7497629)),0),4) 
+
+		#original test diode code
+		#code=int2phex(int(round((6.99655*HVval+4.125)),4) 		
    		return code
 
 # Convert User temp to hex code
 def temp2hex(temp):
 	# Check for invalid temps
+	temp=int(temp)
 	if (temp > 50) or (temp < -50):
 		print "User error, stupid temperature choice. Does not compute. Please try again."	
 		return '0000'
 	else:
    		r=Rthermistor10k(temp)
    		rdivide=2.5*(r/(r+65000))
-   		code=int2phex(v2code(rdivide))
+   		code=int2phex(v2code(rdivide),4)
    		return code
 
 # Convert User comparitor level to hex code
@@ -69,8 +100,64 @@ def comp2hex(comp):
 		print "User error, stupid threshold choice. Does not compute. Please try again."	
 		return '0000'
 	else:  
-   		code=int2phex(v2code(comp/211.5))
+   		code=int2phex(v2code(comp/211.5),4)
    		return code
+
+
+
+
+'''
+ADC converting functions
+'''
+# Convert User hex code to HV
+def hex2volt(hexval,channel):
+	intcode=2*hex10bitcode(hexval)
+	if (channel=='1'):
+		volt=0.14778832632815772*intcode-0.011304923293643755
+
+   	elif (channel=='2'):
+		volt=0.14913241674799574*intcode+0.26857746986763315
+
+	elif (channel=='3'):
+		volt=0.14835849676737672*intcode+0.6480556230452933 
+
+	elif (channel=='4'):
+		volt=0.14865430420790374*intcode-0.408404208038313
+		
+	#original detector equation
+   	#volt=0.285855*intcode-0.5896118255011659	
+   	return round(volt,3)
+
+# Convert User hex code to temp
+def hex2temp(hexval):
+		intcode=hex10bitcode(hexval)
+		volt=code2volt(intcode,5)
+		voltdivider=(volt*65000)/(2.5-volt)
+		temp=Tthermistor10k(voltdivider)
+   		return round(temp,2)
+
+# Convert User hex code to TEC voltage
+def hex2TECV(hexval):
+		intcode=hex10bitcode(hexval)
+		volt=code2volt(intcode,5)
+   		return round(volt,3)
+
+# Convert User hex code to TEC current
+def hex2TECI(hexval):
+		intcode=hex10bitcode(hexval)
+		volt=code2volt(intcode,5)
+		current=volt/(0.05+0.27)
+   		return round(current,3)
+
+
+
+
+
+
+
+
+
+
 
 '''
 DAC testing
@@ -94,45 +181,6 @@ DAC testing
 # compout = comp2hex( comp );
 # print "Comparitor threshold codeword: ", compout
 
-
-'''
-ADC converting functions
-'''
-#switch to 10 bit readout 
-def hex10bitcode(hex16):
-	return int(bin(int(hex16,16))[2:].zfill(16)[4:],2)
-#convert 10 bit codeword to voltage
-def code2volt(code10bit,adcrange):
-	return ((adcrange-2*lsb)/(4096))*code10bit+lsb
-
-# Convert User hex code to HV
-def hex2volt(hexval):
-		intcode=hex10bitcode(hexval)
-   		volt=0.285855*intcode-0.5896118255011659	
-   		return round(volt,3)
-
-# Convert User hex code to temp
-def hex2temp(hexval):
-		intcode=hex10bitcode(hexval)
-		volt=code2volt(intcode,5)
-		print volt
-		voltdivider=(volt*65000)/(2.5-volt)
-		temp=Tthermistor10k(voltdivider)
-   		return round(temp,2)
-
-# Convert User hex code to TEC voltage
-def hex2TECV(hexval):
-		intcode=hex10bitcode(hexval)
-		volt=code2volt(intcode,5)
-   		return round(volt,3)
-
-# Convert User hex code to TEC voltage
-def hex2TECI(hexval):
-		intcode=hex10bitcode(hexval)
-		volt=code2volt(intcode,5)
-		current=volt/(0.05+0.27)
-   		return round(current,3)
-
 '''
 ADC testing
 '''
@@ -151,10 +199,10 @@ ADC testing
 # vout = hex2TECV( v );
 # print "TEC Voltage (V): ", vout
 
-#Testing the TEC current readout
-v= raw_input('Readout the TEC current:\n');
-vout = hex2TECI( v );
-print "TEC current (A): ", vout
+# #Testing the TEC current readout
+# v= raw_input('Readout the TEC current:\n');
+# vout = hex2TECI( v );
+# print "TEC current (A): ", vout
 
 
 
@@ -168,271 +216,3 @@ print "TEC current (A): ", vout
 
 
 
-'''
-
-
-
-
-
-
-print ""
-print "Initializing the Aardvark I2C/SPI controller..."
-print ""
-
-# Find a port with an Aardvark device
-(num, ports, unique_ids) = aa_find_devices_ext(16, 16)
-
-port = int(ports[0])
-
-if num > 0:
-	print "Device found on port " + str(ports[0])
-	#print "Unique device ID: " + str(unique_ids[0])
-
-print ""
-print "Attempting to initialize..."
-
-# Try to open the port for the device
-handle = aa_open(int(ports[0]))
-if (handle <= 0):
-	print "Error, could not initialize the device."
-	print handle
-	sys.exit()
-else:
-	print "Initialization successful, device handle = %d" % handle
-	print ""
-
-# Configure the SPI subsystem to use GPIO and I2C
-aa_configure(handle, AA_CONFIG_GPIO_I2C)
-
-# Ask user for the address
-address = int(raw_input("Enter the slave address of the controller as an integer: "))
-
-# Initialize the controller: send string A0100900F0
-print "Turning various things on..."
-
-init_command = array('B', "A0100900F0") 
-
-bytes_written = aa_i2c_write(handle, address, AA_I2C_NO_FLAGS, init_command)
-
-if bytes_written < 0:
-	print "Error: %s" % aa_status_string(bytes_written)
-elif bytes_written == 0:
-	print "No bytes written."
-elif bytes_written != len(init_command):
-	print "Only %d of %d possible bytes written" % (bytes_written, len(init_command))
-
-while True:
-	print "Menu, written just for you by Olivia. Please choose one of the following menu options: "
-	print "V  :  Set a high voltage"
-	print "T  :  Set temperature"
-	print "C  :  Set comparator threshold"
-	print "1  :  Read TEC current"
-	print "2  :  Read TEC voltage"
-	print "3  :  Read high voltage"
-	print "4  :  Read temperature"
-	print "Q  :  Quit the menu :("
-
-	user_choice = raw_input("Choice: ")
-
-	if (user_choice == 'V') or (user_choice == 'v'):
-		input_voltage = raw_input("Please enter a voltage (in V): ")
-
-		# Check for invalid voltages
-		while (input_voltage > 500) or (input_voltage < 0):
-			print "User error, stupid voltage choice. Does not compute. Please try again."	
-			input_voltage = raw_input("Please try entering a voltage again (in V): ")
-
-		# Turn into an int 
-		rounded_voltage = math.floor(4096*float(input_voltage)/500)
-		# Get the hex string value
-		vhexval_raw = hex(rounded_voltage)
-
-		# Remove the initial '0x'
-		vhexval = vhexval_raw[2:]
-		# Pad with 0s at the beginning to get a 4 bit string
-		while len(vhexval) < 4:
-			vhexval = "0" + vhexval
-
-		# Command to send to controller
-		command = array('B', "03" + vhexval)
-		
-		# Send command
-		bytes_written = aa_i2c_write(handle, address, AA_I2C_NO_FLAGS, command)
-		
-		if bytes_written < 0:
-			print "Error: %s" % aa_status_string(bytes_written)
-			break
-		elif bytes_written == 0:
-			print "No bytes written."
-			break
-		elif bytes_written != len(command):
-			print "Only %d of %d possible bytes written" % (bytes_written, len(command)) 
-
-
-	elif (user_choice == 'T') or (user_choice == 't'):
-		temperature = raw_input("Please enter a temperature (in deg C): ")
-
-		# Check for invalid temperatures
-		while (temperature > 40) or (temperature < -40):
-			if temperature > 40:
-				print "Ouch! Too hot!"
-				temperature = raw_input("Please enter a temperature (in deg C): ")
-			else:
-				print "Oh no, too cold." 
-				temperature = raw_input("Please enter a temperature (in deg C): ")
-
-		# Convert user input (assumed integer) to hex
-		thexval_raw = hex(temperature) 
-		
-		# Format the hex string by removing '0x' and pad beginning with 0
-		thexval = thexval_raw[2:]
-		while len(thexval) < 4:
-			thexval = "0" + thexval
-
-		# Command to send to controller
-		command = array('B', "01" + thexval)
-
-		# Send command
-		bytes_written = aa_i2c_write(handle, address, AA_I2C_NO_FLAGS, command)
-		
-		if bytes_written < 0:
-			print "Error: %s" % aa_status_string(bytes_written)
-			break
-		elif bytes_written == 0:
-			print "No bytes written."
-			break
-		elif bytes_written != len(command):
-			print "Only %d of %d possible bytes written" % (bytes_written, len(command)) 
-
-
-	elif (user_choice == 'C') or (user_choice == 'c'):
-		comp_threshold = raw_input("Please enter a value for the comparator threshold (in mV): ")
-
-		# Check for invalid comparator threshold voltage
-		while (comp_threshold > 500) or (comp_threshold < 0):
-			print "User error, stupid comparator threshold choice. Try again, if you dare..."	
-			input_voltage = raw_input("Please try entering a threshold again (in mV): ")
-
-		# Turn into an int
-		rounded_comp_threshold = math.floor(comp_threshold)
-		# Convert to hex
-		chexval_raw = hex(rounded_comp_threshold)
-
-		# Format the hex string by removing '0x' and pad beginning with 0
-		chexval = chexval_raw[2:]
-		while len(chexval) < 4:
-			chexval = "0" + chexval 
-		
-		# Command to send to controller
-		command = array('B', "02" + chexval)
-
-		# Send command
-		bytes_written = aa_i2c_write(handle, address, AA_I2C_NO_FLAGS, command)
-		
-		if bytes_written < 0:
-			print "Error: %s" % aa_status_string(bytes_written)
-			break
-		elif bytes_written == 0:
-			print "No bytes written."
-			break
-		elif bytes_written != len(command):
-			print "Only %d of %d possible bytes written" % (bytes_written, len(command)) 
-
-
-	elif user_choice == '1':
-		print "Reading TEC current..."
-		
-		# Send a command to write-only box asking for a value to be sent to readout register
-		command = "000101"
-
-		# Read from the box
-		(bytes_read, data_in) = aa_i2c_read(handle, address, AA_I2C_NO_FLAGS, 2)
-
-		if bytes_read < 0:
-			print "Error: %s" % aa_status_string(bytes_read)
-			break
-		elif bytes_read == 0:
-			print "Error: no bytes read."
-			break
-		elif bytes_read != 2:
-			print "Error: 2 bytes expected, only %d received" % bytes_read
-			break
-		else:
-			print "Result: " + str(data_in)
-
-	
-	elif user_choice == '2':
-		print "Reading TEC voltage..."
-
-		command = "000201"
-
-		# Read from the box
-		(bytes_read, data_in) = aa_i2c_read(handle, address, AA_I2C_NO_FLAGS, 2)
-
-		if bytes_read < 0:
-			print "Error: %s" % aa_status_string(bytes_read)
-			break
-		elif bytes_read == 0:
-			print "Error: no bytes read."
-			break
-		elif bytes_read != 2:
-			print "Error: 2 bytes expected, only %d received" % bytes_read
-			break
-		else:
-			print "Result: " + str(data_in)
-
-
-	elif user_choice == '3':
-		print "Reading high voltage..."
-
-		command = "000401"
-
-		# Read from the box
-		(bytes_read, data_in) = aa_i2c_read(handle, address, AA_I2C_NO_FLAGS, 2)
-
-		if bytes_read < 0:
-			print "Error: %s" % aa_status_string(bytes_read)
-			break
-		elif bytes_read == 0:
-			print "Error: no bytes read."
-			break
-		elif bytes_read != 2:
-			print "Error: 2 bytes expected, only %d received" % bytes_read
-			break
-		else:
-			print "Result: " + str(data_in)
-
-
-	elif user_choice == '4':
-		print "Reading temperature..."
-
-		command = "000801"
-
-		# Read from the box
-		(bytes_read, data_in) = aa_i2c_read(handle, address, AA_I2C_NO_FLAGS, 2)
-
-		if bytes_read < 0:
-			print "Error: %s" % aa_status_string(bytes_read)
-			break
-		elif bytes_read == 0:
-			print "Error: no bytes read."
-			break
-		elif bytes_read != 2:
-			print "Error: 2 bytes expected, only %d received" % bytes_read
-			break
-		else:
-			print "Result: " + str(data_in)
-
-
-	elif user_choice == 'Q' or 'q':
-		print "Goodbye!"
-		sys.exit()
-
-	else:
-		print "Invalid option, try again."
-
-
-
-print "Exiting, closing Aardvark device."
-aa_close(handle)
-'''
---
+++ i2crun.py
@@ -0,0 +1,279 @@
+import sys
+import math
+import array
+import smbus
+import i2chelp
+import time
+
+
+print "\n\nInitializing the I2C controller..."
+
+
+bus = smbus.SMBus(1)
+
+#Write a string of values to a register
+def send_command(address,register,command):
+	commandlength = len(command)
+	#register = hex(register)
+	listcommand = [int("".join([command[i],command[i+1]]),16) for i in xrange(0,commandlength-1,2)]
+	
+	#print[commandlength,address,type(address),register,type(register),command,type(command), listcommand,type(listcommand)]
+	bus.write_i2c_block_data(address,register,listcommand)
+
+#read command
+def read_command(address,register,length):
+	return bus.read_i2c_block_data(address,register,length)
+
+
+
+
+
+# Ask user for the address (not gonna for now)
+
+#address = int(raw_input("Enter the slave address of the controller as an hex number: "),16)
+add_1=0x6a
+add_2=0x6b
+add_3=0x6c
+add_4=0x6d
+address=[add_1,add_2,add_3,add_4]
+
+# Initialize the controller: send string 0A100900F0
+
+initcode = [0x10,0x09,0x00,0xf0]
+for i in xrange(0,4):
+	bus.write_i2c_block_data(address[i],0x0A,initcode)
+print "Done!\n"
+
+channelnames = ['1','2','3','4','a','A']
+singlechannel = ['1','2','3','4']
+
+while True:
+	print "-------------------------------------------------\nPlease choose one of the following menu options: \n-------------------------------------------------"
+	print "V  :  Set a bias voltage"
+	print "T  :  Set temperature"
+	print "C  :  Set comparator threshold"
+	print "1  :  Read TEC current"
+	print "2  :  Read TEC voltage"
+	print "3  :  Read high voltage"
+	print "4  :  Read temperature"
+	print "I  :  Initialize the detectors to standard operating settings"
+	print "S  :  Shut down the detectors"
+	print "Q  :  Quit the menu :("
+	user_choice = raw_input("Choice: ")
+
+
+####Setting the bias voltage
+	if (user_choice == 'V') or (user_choice == 'v'):
+		vchannel = raw_input("Please select a channel number (or a to set all at once): ")
+		while vchannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			vchannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if vchannel in singlechannel:
+			vaddress=[address[int(vchannel)-1]]
+		else:
+			vaddress = address
+			vchannel = singlechannel
+
+		input_voltage = float(raw_input("Please enter a voltage (in V): "))
+		
+		for i in xrange(0,len(vaddress)):
+			vcode = i2chelp.volt2hex(input_voltage,vchannel[i])
+			send_command(vaddress[i],0x02,vcode)
+			print 'Voltage on channel '+str(vchannel[i])+' set to '+str(input_voltage)+'V'		
+		print '\n'
+
+####Setting the temperature
+	elif (user_choice == 'T') or (user_choice == 't'):
+		tchannel = raw_input("Please select a channel number (or a to set all at once): ")
+		while tchannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			tchannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if tchannel in singlechannel:
+			taddress=[address[int(tchannel)-1]]
+		else:
+			taddress = address
+			tchannel = singlechannel
+
+		temperature = float(raw_input("Please enter a temperature (in deg C): "))
+
+		tcode = i2chelp.temp2hex(temperature)
+		print '\n'
+		for i in xrange(0,len(taddress)):
+			send_command(taddress[i],0x01,tcode)	
+			print 'Temperature on channel '+str(tchannel[i])+' set to '+str(temperature)+'C'
+		print '\n'
+
+
+######set comparitor threshold level
+	elif (user_choice == 'C') or (user_choice == 'c'):
+		cchannel = raw_input("Please select a channel number (or a to set all at once): ")
+		while cchannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			cchannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if cchannel in singlechannel:
+			caddress=[address[int(cchannel)-1]]
+		else:
+			caddress = address
+			cchannel = singlechannel
+
+		comp_threshold = float(raw_input("Please enter a value for the comparator threshold (in mV): "))
+		ccode = i2chelp.comp2hex(comp_threshold)
+		print '\n'
+		for i in xrange(0,len(caddress)):
+			send_command(caddress[i],0x03,ccode)	
+			print 'Threshold on channel '+str(cchannel[i])+' set to '+str(comp_threshold)+'mV'
+		print '\n'
+
+
+#####Read the TEC current
+	elif user_choice == '1':
+		ichannel = raw_input("Please select a channel number (or a to read all at once): ")
+		while ichannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			ichannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if ichannel in singlechannel:
+			iaddress=[address[int(ichannel)-1]]
+		else:
+			iaddress = address
+			ichannel = singlechannel
+		print "Reading TEC current..."
+		readicommand = '01'
+		iread=range(len(iaddress))
+		for i in xrange(0,len(iaddress)):		
+			send_command(iaddress[i],0x00,readicommand)
+			idata=read_command(iaddress[i],0x01,2)
+			idata=i2chelp.hex2TECI(i2chelp.list2string(idata))
+			iread[i] = ['Channel '+ str(ichannel[i])+': ',idata]	
+
+		print "\nResult: " 
+		for i in xrange(0,len(iread)):
+			print iread[i][0]+str(iread[i][1])+' A'
+		print "\n"
+
+
+
+	
+	elif user_choice == '2':
+		vchannel = raw_input("Please select a channel number (or a to read all at once): ")
+		while vchannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			vchannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if vchannel in singlechannel:
+			vaddress=[address[int(vchannel)-1]]
+		else:
+			vaddress = address
+			vchannel = singlechannel
+		print "Reading TEC voltage..."
+		readvcommand = '02'
+		vread=range(len(vaddress))
+		for i in xrange(0,len(vaddress)):		
+			send_command(vaddress[i],0x00,readvcommand)
+			vdata=read_command(vaddress[i],0x01,2)
+			vdata=i2chelp.hex2TECV(i2chelp.list2string(vdata))
+			vread[i] = ['Channel '+ str(vchannel[i])+': ',vdata]	
+
+		print "\nResult: " 
+		for i in xrange(0,len(vread)):
+			print vread[i][0]+str(vread[i][1])+' V'
+		print "\n"
+		
+
+
+	elif user_choice == '3':
+		hvchannel = raw_input("Please select a channel number (or a to read all at once): ")
+		while hvchannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			hvchannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if hvchannel in singlechannel:
+			hvaddress=[address[int(hvchannel)-1]]
+		else:
+			hvaddress = address
+			hvchannel = singlechannel
+		print "Reading high voltage..."
+		readHVcommand = '04'
+		HVread=range(len(hvaddress))
+		for i in xrange(0,len(hvaddress)):		
+			send_command(hvaddress[i],0x00,readHVcommand)
+			HVdata=read_command(hvaddress[i],0x01,2)
+			HVdata=i2chelp.hex2volt(i2chelp.list2string(HVdata),hvchannel[i])
+			HVread[i] = ['Channel '+ str(hvchannel[i])+': ',HVdata]	
+
+		print "\nResult: " 
+		for i in xrange(0,len(HVread)):
+			print HVread[i][0]+str(HVread[i][1])+' V'
+		print "\n"
+
+#Reading out what the temperature is 
+	elif user_choice == '4':
+		Tchannel = raw_input("Please select a channel number (or a to read all at once): ")
+		while Tchannel not in channelnames:
+			print "User error, bad channel number choice. Choose from [1,2,3,4,a]"
+			Tchannel = raw_input("Please select a channel number (or A to set all at once): ")
+		if Tchannel in singlechannel:
+			Taddress=[address[int(Tchannel)-1]]
+		else:
+			Taddress = address
+			Tchannel = singlechannel
+		print "Reading temperature..."
+		readTcommand = '08'
+		Tread=range(len(Taddress))
+		
+		for i in xrange(0,len(Taddress)):		
+			send_command(Taddress[i],0x00,readTcommand)
+			Tdata=read_command(Taddress[i],0x01,2)
+			Tdata=i2chelp.hex2temp(i2chelp.list2string(Tdata))
+			Tread[i] = ['Channel '+ str(Tchannel[i])+': ',Tdata]	
+
+		print "\nResult: " 
+		for i in xrange(0,len(Tread)):
+			print Tread[i][0]+str(Tread[i][1])+'C'
+		print "\n"
+
+
+#Shutdown the channels
+	elif (user_choice == 'S') or (user_choice == 's'):
+		print "Commencing shutdown procedures..."
+		#set temps to 0:
+		for i in xrange(0,len(address)):
+			send_command(address[i],0x01,'0000')	
+			print 'Temperature on channel '+str(singlechannel[i])+' set to room temp (25 C)'
+		#set HV to 0:
+		for i in xrange(0,len(address)):
+			send_command(address[i],0x02,'0000')	
+			print 'Bias voltage on channel '+str(singlechannel[i])+' set to 0 V'
+		#set comparitor to 0:
+		for i in xrange(0,len(address)):
+			send_command(address[i],0x03,'0000')	
+			print 'Threshold on channel '+str(singlechannel[i])+' set to 0 mV'
+		print "Done!"
+
+#Initialize the channels
+	elif (user_choice == 'I') or (user_choice == 'i'):
+		print "Commencing startup procedures..."
+		#set temps to 0:
+		for i in xrange(0,len(address)):
+			send_command(address[i],0x01,'09C5')
+			time.sleep(8)
+			print 'Temperature on channel '+str(singlechannel[i])+' set to -20C'
+		#set HV to 0:
+		workingvbias=[338,345,360,358]
+		for i in xrange(0,len(address)):
+			send_command(address[i],0x02,i2chelp.volt2hex(workingvbias[i],singlechannel[i]))	
+			print 'Bias voltage on channel '+str(singlechannel[i])+' set to '+str(workingvbias[i])
+		#set comparitor to 150mV:
+		for i in xrange(0,len(address)):
+			send_command(address[i],0x03,'048B')	
+			print 'Threshold on channel '+str(singlechannel[i])+' set to 50 mV'
+		print "Done!"
+
+
+	elif user_choice == 'Q' or 'q':
+		print "Goodbye!"
+		sys.exit()
+
+	else:
+		print "Invalid option, try again."
+
+
+
+
